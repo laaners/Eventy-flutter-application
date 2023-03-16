20b8c3cedd76b9684181ec63f7b8e78b2646e7a1
Alessio Hu <kirbyalessio@yahoo.it>
2023-03-16 10:42:05 +0100

    Pull

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -21,186 +21,197 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: const MyAppBar("Home"),
-      body: ListView(
+      body: Column(
         children: [
           const Text("ok"),
-          Consumer<FirebaseUser>(
-            builder: (context, value, child) {
-              return Text("${value.user}");
-            },
-          ),
-          Consumer<FirebaseUser>(
-            builder: (context, value, child) {
-              return Text(value.userData.toString());
-            },
-          ),
+          Expanded(
+            child: ListView(
+              children: [
+                const Text("ok"),
+                Consumer<FirebaseUser>(
+                  builder: (context, value, child) {
+                    return Text("${value.user}");
+                  },
+                ),
+                Consumer<FirebaseUser>(
+                  builder: (context, value, child) {
+                    return Text(value.userData.toString());
+                  },
+                ),
 
-          TextFormField(),
-          Text(Provider.of<Something>(context).stringa),
-          // equivalente a quello sopra
-          Text(context.watch<Something>().stringa),
-          Text(Provider.of<String>(context)),
+                TextFormField(),
+                Text(Provider.of<Something>(context).stringa),
+                // equivalente a quello sopra
+                Text(context.watch<Something>().stringa),
+                Text(Provider.of<String>(context)),
 
-          // mutable provider
-          // Wrap ONLY the part I want to rebuild in a Consumer of provider type, watch rebuilds everything
-          Consumer<CounterProviderSample>(
-            builder: (context, providerObj, child) {
-              return Text("${providerObj.counter}");
-            },
-          ),
-          ElevatedButton(
-            onPressed: () {
-              Provider.of<CounterProviderSample>(context, listen: false)
-                  .incrementCounter();
-              // Read inside build should be avoided
-              // context.read<CounterProviderSample>().incrementCounter();
-            },
-            child: const Icon(Icons.add),
-          ),
+                // mutable provider
+                // Wrap ONLY the part I want to rebuild in a Consumer of provider type, watch rebuilds everything
+                Consumer<CounterProviderSample>(
+                  builder: (context, providerObj, child) {
+                    return Text("${providerObj.counter}");
+                  },
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    Provider.of<CounterProviderSample>(context, listen: false)
+                        .incrementCounter();
+                    // Read inside build should be avoided
+                    // context.read<CounterProviderSample>().incrementCounter();
+                  },
+                  child: const Icon(Icons.add),
+                ),
 
-          ElevatedButton(
-              child: const Text('Open search'),
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const SearchPage()),
-                );
-              }),
+                ElevatedButton(
+                    child: const Text('Open search'),
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const SearchPage()),
+                      );
+                    }),
 
-          //Stream provider
-          Consumer<int>(
-            builder: (context, providerObj, child) {
-              return Text("$providerObj");
-            },
-          ),
+                //Stream provider
+                Consumer<int>(
+                  builder: (context, providerObj, child) {
+                    return Text("$providerObj");
+                  },
+                ),
 
-          //Future provider
-          Consumer<String>(
-            builder: (context, providerObj, child) {
-              final address = providerObj;
-              return Text("$address dsadsa");
-            },
-          ),
+                //Future provider
+                Consumer<String>(
+                  builder: (context, providerObj, child) {
+                    final address = providerObj;
+                    return Text("$address dsadsa");
+                  },
+                ),
 
-          // DB test
-          TextButton(
-            onPressed: () async {
-              /*
-              for (var i = 10; i < 30; i++) {
-                await Provider.of<FirebaseUser>(context, listen: false)
-                    .signUpWithEmail(
-                  email: "test$i@test.it",
-                  password: "password",
-                  username: "Username$i",
-                  name: "name$i",
-                  surname: "surname$i",
-                  profilePic: "profilePic",
-                  context: context,
-                );
-              }
-              */
-              var curUid =
-                  Provider.of<FirebaseUser>(context, listen: false).user!.uid;
-              Provider.of<FirebaseFollow>(context, listen: false)
-                  .addFollower(context, curUid, "1", true);
-              Provider.of<FirebaseFollow>(context, listen: false)
-                  .removeFollower(context, curUid, "1", true);
-            },
-            child: const Text("Firebase test"),
-          ),
+                // DB test
+                TextButton(
+                  onPressed: () async {
+                    /*
+                    for (var i = 10; i < 30; i++) {
+                      await Provider.of<FirebaseUser>(context, listen: false)
+                          .signUpWithEmail(
+                        email: "test$i@test.it",
+                        password: "password",
+                        username: "Username$i",
+                        name: "name$i",
+                        surname: "surname$i",
+                        profilePic: "profilePic",
+                        context: context,
+                      );
+                    }
+                    */
+                    var curUid =
+                        Provider.of<FirebaseUser>(context, listen: false)
+                            .user!
+                            .uid;
+                    Provider.of<FirebaseFollow>(context, listen: false)
+                        .addFollower(context, curUid, "1", true);
+                    Provider.of<FirebaseFollow>(context, listen: false)
+                        .removeFollower(context, curUid, "1", true);
+                  },
+                  child: const Text("Firebase test"),
+                ),
 
-          // Search user functionality
-          TextButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const SearchPage()),
-                );
-              },
-              child: const Text("Search page")),
+                // Search user functionality
+                TextButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const SearchPage()),
+                      );
+                    },
+                    child: const Text("Search page")),
 
-          TextButton(
-            onPressed: () async {
-              LoadingOverlay.show(context);
-              await Provider.of<FirebaseUser>(context, listen: false)
-                  .loginWithEmail(
-                email: "test13@test.it", //"ok@ok.it",
-                password: "password",
-                context: context,
-              );
-              LoadingOverlay.hide(context);
-            },
-            child: const Text("Firebase login"),
-          ),
-          TextButton(
-            onPressed: () async {
-              LoadingOverlay.show(context);
-              // overlay.show();
-              var document = await FirebaseCrud.readDoc(
-                Provider.of<FirebaseUser>(context, listen: false)
-                    .userCollection,
-                "IrI8s7a6WeVUgF3fAYd99YHdnqh2",
-              );
-              LoadingOverlay.hide(context);
-              UserCollection usertest = UserCollection.fromMap(
-                document?.data() as Map<String, dynamic>,
-              );
-              print(document?.exists);
-              print(usertest.email);
-              // overlay.hide();
+                TextButton(
+                  onPressed: () async {
+                    LoadingOverlay.show(context);
+                    await Provider.of<FirebaseUser>(context, listen: false)
+                        .loginWithEmail(
+                      email: "test13@test.it", //"ok@ok.it",
+                      password: "password",
+                      context: context,
+                    );
+                    LoadingOverlay.hide(context);
+                  },
+                  child: const Text("Firebase login"),
+                ),
+                TextButton(
+                  onPressed: () async {
+                    LoadingOverlay.show(context);
+                    // overlay.show();
+                    var document = await FirebaseCrud.readDoc(
+                      Provider.of<FirebaseUser>(context, listen: false)
+                          .userCollection,
+                      "IrI8s7a6WeVUgF3fAYd99YHdnqh2",
+                    );
+                    LoadingOverlay.hide(context);
+                    UserCollection usertest = UserCollection.fromMap(
+                      document?.data() as Map<String, dynamic>,
+                    );
+                    print(document?.exists);
+                    print(usertest.email);
+                    // overlay.hide();
 
-              var snapshots = await FirebaseCrud.readSnapshot(
-                Provider.of<FirebaseUser>(context, listen: false)
-                    .userCollection,
-                "IrI8s7a6WeVUgF3fAYd99YHdnqh2",
-              );
-              snapshots?.listen(
-                (event) => print("current data: ${event.data()}"),
-                onError: (error) => print("Listen failed: $error"),
-              );
-            },
-            child: const Text("Firebase get"),
-          ),
-          TextButton(
-            onPressed: () async {
-              await Provider.of<FirebaseUser>(context, listen: false)
-                  .signOut(context);
-            },
-            child: const Text("Firebase sign out"),
-          ),
-          TextButton(
-            onPressed: () async {
-              await Provider.of<FirebaseUser>(context, listen: false)
-                  .deleteAccount(context);
-            },
-            child: const Text("Firebase delete"),
-          ),
-          /*
-          TextButton(
-            onPressed: () async {
-              var rows =
-                  await Provider.of<PostgresMethods>(context, listen: false)
-                      .test(context);
-              for (final row in rows) {
-                debugPrint(row["users"]["username"]);
-              }
-            },
-            child: const Text("Postgres test"),
-          ),
-          */
+                    var snapshots = await FirebaseCrud.readSnapshot(
+                      Provider.of<FirebaseUser>(context, listen: false)
+                          .userCollection,
+                      "IrI8s7a6WeVUgF3fAYd99YHdnqh2",
+                    );
+                    snapshots?.listen(
+                      (event) => print("current data: ${event.data()}"),
+                      onError: (error) => print("Listen failed: $error"),
+                    );
+                  },
+                  child: const Text("Firebase get"),
+                ),
+                TextButton(
+                  onPressed: () async {
+                    await Provider.of<FirebaseUser>(context, listen: false)
+                        .signOut(context);
+                  },
+                  child: const Text("Firebase sign out"),
+                ),
+                TextButton(
+                  onPressed: () async {
+                    await Provider.of<FirebaseUser>(context, listen: false)
+                        .deleteAccount(context);
+                  },
+                  child: const Text("Firebase delete"),
+                ),
+                /*
+                TextButton(
+                  onPressed: () async {
+                    var rows =
+                        await Provider.of<PostgresMethods>(context, listen: false)
+                            .test(context);
+                    for (final row in rows) {
+                      debugPrint(row["users"]["username"]);
+                    }
+                  },
+                  child: const Text("Postgres test"),
+                ),
+                */
 
-          // List DB fetch
-          const UsersList2(),
+                // List DB fetch
+                const UsersList2(),
 
-          // long shape
-          Center(
-            child: Container(
-              color: Colors.orange,
-              width: 20,
-              height: 1000,
+                // long shape
+                Center(
+                  child: Container(
+                    color: Colors.orange,
+                    width: 20,
+                    height: 1000,
+                  ),
+                ),
+                const Text("ok"),
+              ],
             ),
           ),
-          const Text("ok"),
         ],
       ),
     );
@@ -256,9 +267,9 @@ class UsersList2 extends StatelessWidget {
   }
 }
 
-class UserTile extends StatelessWidget {
+class UserTile2 extends StatelessWidget {
   final UserCollection user;
-  const UserTile({super.key, required this.user});
+  const UserTile2({super.key, required this.user});
 
   @override
   Widget build(BuildContext context) {
--- lib/screens/poll_detail/index.dart
+++ lib/screens/poll_detail/index.dart
@@ -4,6 +4,8 @@ import 'package:dima_app/server/tables/poll_collection.dart';
 import 'package:dima_app/transitions/screen_transition.dart';
 import 'package:dima_app/widgets/loading_spinner.dart';
 import 'package:dima_app/widgets/my_app_bar.dart';
+import 'package:dima_app/widgets/pill_box.dart';
+import 'package:dima_app/widgets/user_list.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -30,7 +32,7 @@ class _PollDetailScreenState extends State<PollDetailScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: MyAppBar("Poll ${pollData?.pollName}"),
+      appBar: MyAppBar("Poll Detail"),
       body: FutureBuilder<PollCollection?>(
         future: Provider.of<FirebasePoll>(context, listen: false)
             .getPollData(context, widget.pollId),
@@ -57,12 +59,61 @@ class _PollDetailScreenState extends State<PollDetailScreen> {
           }
           PollCollection pollData = snapshot.data!;
           print(pollData.toString());
-          return ListView(
-            children: [
-              Text(pollData.organizerUid),
-              Text(pollData.deadline),
-              Text(pollData.public.toString()),
-            ],
+          return Container(
+            margin: const EdgeInsets.all(10),
+            child: ListView(
+              children: [
+                Container(
+                  padding: const EdgeInsets.all(5),
+                  child: Center(
+                    child: Text(
+                      pollData.pollName,
+                      style: const TextStyle(
+                        fontSize: 35,
+                        fontWeight: FontWeight.normal,
+                        letterSpacing: 4,
+                      ),
+                    ),
+                  ),
+                ),
+                const Text(
+                  "Organized by",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18,
+                  ),
+                ),
+                UserTile(userUid: pollData.organizerUid),
+                const Text(
+                  "About this event",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18,
+                  ),
+                ),
+                Container(padding: const EdgeInsets.symmetric(vertical: 5)),
+                Text(pollData.pollDesc.isEmpty
+                    ? "The organized did not provide any description"
+                    : pollData.pollDesc),
+                Container(
+                  width: 40,
+                  margin: EdgeInsets.symmetric(horizontal: 50),
+                  child: SizedBox(
+                    child: PillBox(
+                      child: const Text(
+                        "Virtual meeting",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Text(pollData.deadline),
+                Text(pollData.public.toString()),
+              ],
+            ),
           );
         },
       ),
