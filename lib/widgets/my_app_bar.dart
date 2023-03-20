513035a0a3d3740e7af03e0650a70f8888645b62
Gabriele Lazzarelli <47924888+glazzarelli@users.noreply.github.com>
2023-03-20 12:09:49 +0100

    update appBar with search option

--- lib/widgets/my_app_bar.dart
+++ lib/widgets/my_app_bar.dart
@@ -2,6 +2,15 @@ import 'package:dima_app/providers/theme_switch.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+import '../screens/error.dart';
+import '../screens/profile/index.dart';
+import '../screens/profile/profile_pic.dart';
+import '../screens/profile/view_profile.dart';
+import '../server/firebase_user.dart';
+import '../server/tables/user_collection.dart';
+import '../transitions/screen_transition.dart';
+import 'loading_spinner.dart';
+
 class MyAppBar extends StatelessWidget implements PreferredSizeWidget {
   @override
   Size get preferredSize => const Size.fromHeight(50);
@@ -13,17 +22,123 @@ class MyAppBar extends StatelessWidget implements PreferredSizeWidget {
     return AppBar(
       centerTitle: true,
       title: Text(title),
-      // actions: [
-      //   TextButton(
-      //     onPressed: () {
-      //       Provider.of<ThemeSwitch>(context, listen: false).changeTheme();
-      //     },
-      //     child: Icon(
-      //       Icons.dark_mode,
-      //       color: Provider.of<ThemeSwitch>(context).themeData.iconTheme.color,
-      //     ),
-      //   ),
-      // ],
+      backgroundColor: Colors.transparent,
+      actions: [
+        TextButton(
+          onPressed: () async {
+            await showSearch<String>(
+              context: context,
+              delegate: CustomDelegate(),
+            );
+          },
+          child: Icon(
+            Icons.search,
+            color: Provider.of<ThemeSwitch>(context).themeData.iconTheme.color,
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class CustomDelegate extends SearchDelegate<String> {
+  List<UserCollection> usersData = [];
+
+  @override
+  List<Widget> buildActions(BuildContext context) =>
+      [IconButton(icon: const Icon(Icons.clear), onPressed: () => query = '')];
+
+  @override
+  Widget buildLeading(BuildContext context) => IconButton(
+        icon: const Icon(Icons.chevron_left),
+        onPressed: () => close(context, ''),
+      );
+
+  @override
+  Widget buildResults(BuildContext context) => Container();
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    return query.isEmpty
+        ? Container()
+        : FutureBuilder(
+            future: Provider.of<FirebaseUser>(context, listen: false)
+                .getUsersData(context, query),
+            builder: (
+              context,
+              snapshot,
+            ) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const LoadingSpinner();
+              }
+              if (snapshot.hasError) {
+                Future.microtask(() {
+                  Navigator.of(context).pop();
+                  Navigator.push(
+                    context,
+                    ScreenTransition(
+                      builder: (context) => ErrorScreen(
+                        errorMsg: snapshot.error.toString(),
+                      ),
+                    ),
+                  );
+                });
+                return Container();
+              }
+              if (!snapshot.hasData) {
+                return Container();
+              }
+              usersData = snapshot.data!;
+              return ListView.builder(
+                itemCount: usersData.length,
+                itemBuilder: (_, i) {
+                  var user = usersData[i];
+                  return UserTile(
+                    userData: user,
+                  );
+                },
+              );
+            },
+          );
+  }
+}
+
+class UserTile extends StatelessWidget {
+  final UserCollection userData;
+  const UserTile({super.key, required this.userData});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 80,
+      child: ListTile(
+        leading: ProfilePic(
+          loading: false,
+          userData: userData,
+          radius: 30,
+        ),
+        title: Text("${userData.name} ${userData.surname}"),
+        subtitle: Text(userData.username),
+        onTap: () {
+          var curUid =
+              Provider.of<FirebaseUser>(context, listen: false).user!.uid;
+          if (curUid == userData.uid) {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => const ProfileScreen(),
+              ),
+            );
+          } else {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => ViewProfileScreen(userData: userData),
+              ),
+            );
+          }
+        },
+      ),
     );
   }
 }
