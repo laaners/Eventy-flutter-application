fb906107af4d3ebf307d721b5fdb929d21dc7b83
Gabriele Lazzarelli <47924888+glazzarelli@users.noreply.github.com>
2023-04-14 13:35:25 +0200

    Add layout constants

---
+++ lib/themes/layout_constants.dart
@@ -0,0 +1,32 @@
+/* Constants class for layout description */
+
+class LayoutConstants {
+  /// Horizontal padding of the screen
+  static const double kHorizontalPadding = 20.0;
+
+  /// Vertical padding of the screen
+  static const double kVerticalPadding = 30.0;
+
+  /// Profile Picture radius
+  static const double kProfilePicRadius = 90.0;
+
+  /// Divider height
+  static const double kDividerHeight = 30.0;
+
+  /// Padding between widgets
+  static const double kHeight = 20.0;
+
+  /// Button height used for text buttons
+  static const double kButtonHeight = 30.0;
+
+  /// Button width used for text buttons
+  static const double kButtonWidth = 110.0;
+
+  static const double kIconSize = 20.0;
+  static const double kIconSizeSmall = 15.0;
+  static const double kIconSizeLarge = 30.0;
+
+  static const double kAvatarSize = 50.0;
+  static const double kAvatarSizeSmall = 40.0;
+  static const double kAvatarSizeLarge = 60.0;
+}
