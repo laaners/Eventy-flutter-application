643ab5198433443634c65f2c7fff5222b801a45c
Gabriele Lazzarelli <47924888+glazzarelli@users.noreply.github.com>
2023-04-14 13:33:10 +0200

    Profile info modification
    Now it wraps only the username and name

--- lib/screens/profile/profile_info.dart
+++ lib/screens/profile/profile_info.dart
@@ -1,13 +1,5 @@
-import 'package:dima_app/screens/error.dart';
-import 'package:dima_app/server/tables/follow_collection.dart';
-import 'package:dima_app/widgets/loading_spinner.dart';
-import 'package:dima_app/widgets/profile_pic.dart';
 import 'package:dima_app/server/tables/user_collection.dart';
-import 'package:dima_app/transitions/screen_transition.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import '../../server/firebase_follow.dart';
-import 'follow_list.dart';
 
 class ProfileInfo extends StatelessWidget {
   final UserCollection? userData;
@@ -17,94 +9,16 @@ class ProfileInfo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
       children: [
-        Center(
-          child: Column(
-            children: [
-              Container(
-                margin: const EdgeInsets.all(10),
-                child: ProfilePic(
-                  userData: userData,
-                  loading: false,
-                  radius: 90,
-                ),
-              ),
-              Text(
-                '@${userData!.username}',
-                style: Theme.of(context).textTheme.displaySmall,
-              ),
-              Text("${userData?.name} ${userData?.surname}",
-                  style: Theme.of(context).textTheme.headlineSmall),
-            ],
-          ),
+        Text(
+          '@${userData!.username}',
+          style: Theme.of(context).textTheme.displaySmall,
         ),
-        FutureBuilder<FollowCollection>(
-          future: Provider.of<FirebaseFollow>(context, listen: false)
-              .getFollow(userData!.uid),
-          builder: (
-            BuildContext context,
-            AsyncSnapshot<FollowCollection> snapshot,
-          ) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return const LoadingSpinner();
-            }
-            if (snapshot.hasError) {
-              Future.microtask(() {
-                Navigator.of(context).pop();
-                Navigator.push(
-                  context,
-                  ScreenTransition(
-                    builder: (context) => ErrorScreen(
-                      errorMsg: snapshot.error.toString(),
-                    ),
-                  ),
-                );
-              });
-              return Container();
-            }
-            if (!snapshot.hasData) {
-              return Container();
-            }
-            FollowCollection follow = snapshot.data!;
-            return Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                TextButton(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => FollowListScreen(
-                          users: follow.followers,
-                          title: "${userData?.username} Followers",
-                        ),
-                      ),
-                    );
-                  },
-                  child: Text("${follow.followers.length} followers"),
-                ),
-                const VerticalDivider(thickness: 2),
-                TextButton(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => FollowListScreen(
-                          users: follow.following,
-                          title: "${userData?.username} Following",
-                        ),
-                      ),
-                    );
-                  },
-                  child: Text("${follow.following.length} following"),
-                ),
-              ],
-            );
-          },
-        ),
+        Text("${userData?.name} ${userData?.surname}",
+            style: Theme.of(context).textTheme.headlineSmall),
       ],
     );
   }
 }
-
-// todo: add ProfileViewScreen
