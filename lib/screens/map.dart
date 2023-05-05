627ab78f27489b83308e2838fd7618285cf77d2c
Gabriele Lazzarelli <47924888+glazzarelli@users.noreply.github.com>
2023-05-05 12:25:10 +0200

    update map

--- lib/screens/map.dart
+++ lib/screens/map.dart
@@ -1,10 +1,14 @@
 import 'package:dima_app/widgets/my_app_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
+import 'package:location/location.dart';
 
 class MapScreen extends StatefulWidget {
-  static const String route = '/marker_anchors';
+  // Routes are not used in this screen
+  // static const String route = '/marker_anchors';
+  // static const String route = '/live_location';
 
   const MapScreen({Key? key}) : super(key: key);
 
@@ -15,38 +19,127 @@ class MapScreen extends StatefulWidget {
 }
 
 class MapScreenState extends State<MapScreen> {
+  LocationData? _currentLocation;
+  late final MapController _mapController;
   late AnchorPos<dynamic> anchorPos;
+
+  bool _liveUpdate = false;
+  bool _permission = false;
+
+  String? _serviceError = '';
+
+  // Allowed gestures on map
+  int interActiveFlags = InteractiveFlag.pinchZoom |
+      InteractiveFlag.doubleTapZoom |
+      InteractiveFlag.drag |
+      InteractiveFlag.doubleTapZoom;
+
+  final Location _locationService = Location();
 
   @override
   void initState() {
     super.initState();
     anchorPos = AnchorPos.align(AnchorAlign.center);
+    _mapController = MapController();
+    initLocationService();
+  }
+
+  void initLocationService() async {
+    await _locationService.changeSettings(
+      accuracy: LocationAccuracy.high,
+      interval: 1000,
+    );
+
+    LocationData? location;
+    bool serviceEnabled;
+    bool serviceRequestResult;
+
+    try {
+      serviceEnabled = await _locationService.serviceEnabled();
+
+      if (serviceEnabled) {
+        final permission = await _locationService.requestPermission();
+        _permission = permission == PermissionStatus.granted;
+
+        if (_permission) {
+          location = await _locationService.getLocation();
+          _currentLocation = location;
+          _locationService.onLocationChanged
+              .listen((LocationData result) async {
+            if (mounted) {
+              setState(() {
+                _currentLocation = result;
+
+                // If Live Update is enabled, move map center
+                if (_liveUpdate) {
+                  _mapController.move(
+                      LatLng(_currentLocation!.latitude!,
+                          _currentLocation!.longitude!),
+                      _mapController.zoom);
+                }
+              });
+            }
+          });
+        }
+      } else {
+        serviceRequestResult = await _locationService.requestService();
+        if (serviceRequestResult) {
+          initLocationService();
+          return;
+        }
+      }
+    } on PlatformException catch (e) {
+      debugPrint(e.toString());
+      if (e.code == 'PERMISSION_DENIED') {
+        _serviceError = e.message;
+      } else if (e.code == 'SERVICE_STATUS_ERROR') {
+        _serviceError = e.message;
+      }
+      location = null;
+    }
   }
 
   @override
   Widget build(BuildContext context) {
+    // Live location coordinates
+    LatLng currentLatLng;
+
+    // Until currentLocation is initially updated, Widget can locate to 0, 0
+    // by default or store previous location value to show.
+    if (_currentLocation != null) {
+      currentLatLng =
+          LatLng(_currentLocation!.latitude!, _currentLocation!.longitude!);
+    } else {
+      currentLatLng = LatLng(45.4855182, 9.1473723);
+    }
+
     final markers = <Marker>[
+      // Live location marker
+      Marker(
+        width: 80,
+        height: 80,
+        point: currentLatLng,
+        builder: (ctx) => const Icon(Icons.location_pin, color: Colors.red),
+      ),
       Marker(
         width: 80,
         height: 80,
         point: LatLng(51.5, -0.09),
-        builder: (ctx) => const FlutterLogo(),
+        builder: (ctx) => const Icon(Icons.location_pin, color: Colors.blue),
         anchorPos: anchorPos,
       ),
       Marker(
         width: 80,
         height: 80,
         point: LatLng(53.3498, -6.2603),
-        builder: (ctx) => const FlutterLogo(
-          textColor: Colors.green,
-        ),
+        builder: (ctx) => const Icon(Icons.location_pin, color: Colors.blue),
         anchorPos: anchorPos,
       ),
       Marker(
         width: 80,
         height: 80,
         point: LatLng(48.8566, 2.3522),
-        builder: (ctx) => const FlutterLogo(textColor: Colors.purple),
+        builder: (ctx) => const Icon(Icons.location_pin, color: Colors.blue),
         anchorPos: anchorPos,
       ),
     ];
@@ -64,9 +157,23 @@ class MapScreenState extends State<MapScreen> {
               padding: const EdgeInsets.only(top: 8, bottom: 8),
               child: Wrap(
                 children: <Widget>[
+                  // Live location button
                   MaterialButton(
-                    onPressed: () {},
-                    child: const Text('today'),
+                    onPressed: () {
+                      setState(() {
+                        _liveUpdate = !_liveUpdate;
+
+                        if (_liveUpdate) {
+                          ScaffoldMessenger.of(context)
+                              .showSnackBar(const SnackBar(
+                            content: Text('Message on live update'),
+                          ));
+                          // center map on current location
+                          _mapController.move(currentLatLng, 13);
+                        }
+                      });
+                    },
+                    child: const Text("near you"),
                   ),
                   MaterialButton(
                     onPressed: () {},
@@ -81,9 +188,13 @@ class MapScreenState extends State<MapScreen> {
             ),
             Flexible(
               child: FlutterMap(
+                mapController: _mapController,
                 options: MapOptions(
-                  center: LatLng(51.5, -0.09),
+                  maxZoom: 18,
+                  center:
+                      LatLng(currentLatLng.latitude, currentLatLng.longitude),
                   zoom: 5,
+                  interactiveFlags: interActiveFlags,
                 ),
                 children: [
                   TileLayer(
