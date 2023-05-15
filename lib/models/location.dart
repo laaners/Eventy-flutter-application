681bb6a9f3a6eb828df9dfe15af56f40e396ce2e
Gabriele Lazzarelli <47924888+glazzarelli@users.noreply.github.com>
2023-05-15 15:59:08 +0200

    add doc

--- lib/models/location.dart
+++ lib/models/location.dart
@@ -1,12 +1,15 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'dart:convert';
 
+/// Location class for storing location data
 class Location {
   final String name;
   final String site;
   final double lat;
   final double lon;
   final String icon;
+
+  /// Location constructor
   Location(
     this.name,
     this.site,
@@ -15,11 +18,13 @@ class Location {
     this.icon,
   );
 
+  /// Location toString method for printing a Location object
   @override
   String toString() {
     return 'Location(name: $name, site: $site, lat: $lat, lon: $lon, icon: $icon)';
   }
 
+  /// Location copyWith method for copying a Location object
   Location copyWith({
     String? name,
     String? site,
@@ -36,6 +41,7 @@ class Location {
     );
   }
 
+  /// Location toMap method for converting a Location object to a Map<String, dynamic>
   Map<String, dynamic> toMap() {
     return <String, dynamic>{
       'name': name,
@@ -46,6 +52,7 @@ class Location {
     };
   }
 
+  /// Location fromMap method for converting a Map<String, dynamic> to a Location object
   factory Location.fromMap(Map<String, dynamic> map) {
     return Location(
       map['name'] as String,
@@ -56,11 +63,14 @@ class Location {
     );
   }
 
+  /// Location toJson method for converting a Location object to a JSON string
   String toJson() => json.encode(toMap());
 
+  /// Location fromJson method for converting a JSON string to a Location object
   factory Location.fromJson(String source) =>
       Location.fromMap(json.decode(source) as Map<String, dynamic>);
 
+  /// Location operator == method for comparing two Location objects
   @override
   bool operator ==(covariant Location other) {
     if (identical(this, other)) return true;
@@ -72,6 +82,7 @@ class Location {
         other.icon == icon;
   }
 
+  /// Location hashCode method for hashing a Location object
   @override
   int get hashCode {
     return name.hashCode ^
