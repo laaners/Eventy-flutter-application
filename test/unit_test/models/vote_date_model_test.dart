92fc82290ed83818fa0fb276a0369d0fd449091c
Gabriele Lazzarelli <47924888+glazzarelli@users.noreply.github.com>
2023-05-17 10:45:23 +0200

    add test

---
+++ test/unit_test/models/vote_date_model_test.dart
@@ -0,0 +1,114 @@
+import 'package:dima_app/models/availability.dart';
+import 'package:dima_app/models/poll_event_invite_model.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:dima_app/models/vote_date_model.dart';
+import 'package:flutter/foundation.dart';
+
+void main() {
+  group('VoteDateModel', () {
+    final testVote = VoteDateModel(
+      pollId: 'poll1',
+      date: '2023-05-15',
+      start: '08:00',
+      end: '10:00',
+      votes: {
+        'user1': 1,
+        'user2': 2,
+        'user3': 0,
+        'user4': -1,
+      },
+    );
+
+    final testInvites = [
+      // PollEventInviteModel(
+      //     inviteeId: 'user3', inviteeName: 'User 3', status: 1),
+      PollEventInviteModel(pollEventId: "pollEventId0", inviteeId: "user1"),
+      PollEventInviteModel(pollEventId: "pollEventId0", inviteeId: "user2"),
+      PollEventInviteModel(pollEventId: "pollEventId0", inviteeId: "user3"),
+      PollEventInviteModel(pollEventId: "pollEventId0", inviteeId: "user4"),
+    ];
+
+    test('copyWith should copy the VoteDateModel', () {
+      final copiedVote = testVote.copyWith(
+        pollId: 'poll2',
+      );
+
+      expect(copiedVote.pollId, 'poll2');
+      expect(copiedVote.date, '2023-05-15');
+      expect(copiedVote.start, '08:00');
+      expect(copiedVote.end, '10:00');
+      expect(
+          mapEquals(copiedVote.votes, {
+            'user1': 1,
+            'user2': 2,
+            'user3': 0,
+            'user4': -1,
+          }),
+          true);
+    });
+
+    test('toMap should return a map with correct data', () {
+      final voteMap = testVote.toMap();
+
+      expect(voteMap, {
+        'pollId': 'poll1',
+        'date': '2023-05-15',
+        'start': '08:00',
+        'end': '10:00',
+        'votes': {
+          'user1': 1,
+          'user2': 2,
+          'user3': 0,
+          'user4': -1,
+        },
+      });
+    });
+
+    test('fromMap should return a VoteDateModel with correct data', () {
+      final voteMap = {
+        'pollId': 'poll1',
+        'date': '2023-05-15',
+        'start': '08:00',
+        'end': '10:00',
+        'votes': {
+          'user1': 1,
+          'user2': 2,
+          'user3': 0,
+          'user4': -1,
+        },
+      };
+
+      final voteFromMap = VoteDateModel.fromMap(voteMap);
+
+      expect(voteFromMap, testVote);
+    });
+
+    test('getVotesKind should correctly return votes of specific kind', () {
+      final positiveVotes =
+          testVote.getVotesKind(Availability.yes, testInvites, 'user1');
+
+      expect(positiveVotes, {'user2': 2});
+
+      final emptyVotes =
+          testVote.getVotesKind(Availability.empty, testInvites, 'user1');
+
+      expect(emptyVotes, {'user4': -1});
+
+      final notVotes =
+          testVote.getVotesKind(Availability.not, testInvites, 'user1');
+
+      expect(notVotes, {'user3': 0});
+
+      final iffVotes =
+          testVote.getVotesKind(Availability.iff, testInvites, 'user1');
+
+      expect(iffVotes, {'user1': 1});
+    });
+
+    test('getPositiveVotes should correctly return positive votes', () {
+      final positiveVotes = testVote.getPositiveVotes();
+
+      expect(positiveVotes, {'user1': 1, 'user2': 2});
+    });
+  });
+}
---
+++ test/unit_test/models/vote_location_model_test.dart
@@ -0,0 +1,15 @@
+import 'package:test/test.dart'; 
+import 'package:dima_app/models/vote_location_model.dart';
+
+void main() {
+	group(
+		VoteLocationModel, 
+		() {
+			test(
+				'',
+				() async {
+				},
+			);
+		},
+	);
+}
